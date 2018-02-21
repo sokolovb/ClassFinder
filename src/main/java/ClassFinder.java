94e9c7024f2c2071bb9ac1a18f4005528901fe7c
boris <b.sokolov@yadro.com>
2018-02-21 16:38:30 +0300

    fsm must be implemented!

--- src/main/java/ClassFinder.java
+++ src/main/java/ClassFinder.java
@@ -11,6 +11,12 @@ public class ClassFinder {
     }
 
     public static void main(String[] args) {
+
+        if (args.length != 2) {
+            out.printf("Wrong number of input parameters: expected 2, got %d\n", args.length);
+            return;
+        }
+
         try {
             readAllLines(get(args[0])).stream()
                     .map(String::trim)
@@ -52,17 +58,69 @@ public class ClassFinder {
         }
     }
 
+    enum State {
+        lookup,
+        check,
+        found
+    }
+
+    private static State FSM;
+
+    // while process wildcard, there are three states:
+    // - when we've found substr which matches the chars following wc - we keep checking it (State.check)
+    // - when we've not found such one - we try to find it until reach the end of the line (State.lookup)
+    // - when we've found the whole piece of substr and came to the new wc (State.found)
+    //
+
     private static boolean matchesByCapitalOrSmallLetters(String line, String pattern) {
 
-        int j = 0;
-        for (int i = 0; i < line.length(); i++) {
-            if (line.charAt(i) == pattern.charAt(j)) {
-                if (j == pattern.length()-1) {
-                    return true;
+        int line_length = line.length();
+        int pattern_length = pattern.length();
+
+        int offset = 0;
+        int line_index = 0;
+        int pattern_index = 0;
+
+        FSM = State.lookup;
+
+        while (pattern_index < pattern_length && line_index < line_length) {
+
+            if (FSM == State.lookup) {
+                if (line.charAt(line_index) == pattern.charAt(pattern_index)) {
+
+                    if (line_index + 1 < line_length) {
+                        FSM = State.check;
+                        offset++;
+                    } else {
+                        return false;
+                    }
                 }
-                j++;
+                line_index++;
+                continue;
+            }
+
+            if (FSM == State.check) {
+                if (pattern.charAt(pattern_index + offset) == '*') {
+                    if (pattern_length >= pattern_index + offset + 1) {
+                        FSM = State.lookup;
+                        pattern_index += offset + 1;
+                    } else  {
+                        return true;
+                    }
+                }
+                if (line_index + 1 < line_length) {
+                    if (line.charAt(line_index + 1) == pattern.charAt(pattern_index + offset)) {
+                        offset++;
+                    } else {
+                        FSM = State.lookup;
+                    }
+                    line_index++;
+                } else {
+                    return false;
+                }
             }
         }
+
         return false;
     }
 }
